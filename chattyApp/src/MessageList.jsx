aab61f7e8cbe0462d0e784968c8342ef1f267fc6
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-07-05 06:03:42 +0000

    add the function that when there is new msg, scroll to that place

--- chattyApp/src/MessageList.jsx
+++ chattyApp/src/MessageList.jsx
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+  scrollToBottom() {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
   render() {
     const messages = this.props.messages.map((msg) => {
       return (<Message key={msg.id} {...msg} />);
     });
     return (
-      <main className="messages"> {messages} </main>
+      <main className="messages">
+        {messages}
+        <div ref={(el) => { this.messagesEnd = el; }}></div>
+      </main>
     );
   }
 }
